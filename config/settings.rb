ec21f85acf88f5e8e047760451a0c41a0521d4c8
Jim Chambers <mrhistory@gmail.com>
2014-04-22 16:26:46 -0400

    Force SSL in production only.

--- config/settings.rb
+++ config/settings.rb
@@ -1,8 +1,22 @@
 configure :development do
+  set :host, 'localhost:9999'
+  set :force_ssl, false
 end
 
 configure :test do
+  set :host, 'localhost:9999'
+  set :force_ssl, false
 end
 
 configure :production do
+  set :host, 'user-service-dev.herokuapp.com'
+  set :force_ssl, true
+end
+
+before do
+  content_type :json
+  ssl_whitelist = ['/calendar.ics']
+  if settings.force_ssl && !request.secure? && !ssl_whitelist.include?(request.path_info)
+    halt json_status 400, "Please use SSL at https://#{settings.host}"
+  end
 end
